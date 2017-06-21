a48a4d7fac513ef62b0ff27ea898c25cafb78945
chenzhuming <chentianming1209@163.com>
2017-06-21 11:34:16 +0800

    提交定时服务代码

---
+++ WindowsTimeService/WindowsTimeService/param.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace WindowsTimeService
+{
+    [XmlRoot("Paramlist")]
+    public class Paramlist
+    {
+        [XmlElement("Param")]
+        public List<Param> DataSource { get; set; }
+    }
+
+    [XmlType("Param")]
+    public class Param
+    {
+        [XmlAttribute("hour")]
+        public string Hour { get; set; }
+
+        [XmlAttribute("minute")]
+        public string Minute { get; set; }
+
+        [XmlAttribute("url")]
+        public string Url { get; set; }
+    }
+}
