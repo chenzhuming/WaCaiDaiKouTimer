3fbc093260900269059247574367edc9f94b7086
chenzhuming <chentianming1209@163.com>
2017-06-28 16:38:09 +0800

    代扣定时任务

---
+++ 代扣定时服务/WindowsTimeService/InterfaceHelper.cs
@@ -0,0 +1,166 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tellyes.Common
+{
+    public class InterfaceHelper
+    {
+        /// <summary>
+        /// 调用接口通用方法
+        /// </summary>
+        /// <param name="strUri">接口地址</param>
+        /// <param name="strPara">传入参数</param>
+        /// <param name="strResult">返回的json串</param>
+        /// <returns>请求是否成功，1成功，0不成功</returns>
+        public static int RequestService(string strUri, string strPara, ref string strResult)
+        {
+            int intResult = 1;
+            string message = string.Empty;
+            strResult = string.Empty;
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(strUri);
+
+                myRequest.Method = "POST";
+
+                myRequest.ContentType = "application/x-www-form-urlencoded";
+                //myRequest.ContentType = "application/json; charset=utf-8";
+                string paraUrlCoded = strPara;
+                byte[] payload;
+                //将URL编码后的字符串转化为字节
+                payload = System.Text.Encoding.GetEncoding("utf-8").GetBytes(paraUrlCoded);
+                //设置请求的 ContentLength 
+                myRequest.ContentLength = payload.Length;
+                //获得请求流
+                Stream writer = myRequest.GetRequestStream();
+                //将请求参数写入流
+                writer.Write(payload, 0, payload.Length);
+                // 关闭请求流
+                writer.Close();
+
+                try
+                {
+                    HttpWebResponse HttpWResp = (HttpWebResponse)myRequest.GetResponse();
+                    Stream myStream = HttpWResp.GetResponseStream();
+                    StreamReader sr = new StreamReader(myStream, Encoding.GetEncoding("utf-8"));
+                    strResult = sr.ReadToEnd();
+
+                    //将json格式的字符串转换成实例
+                    JObject obj = JsonConvert.DeserializeObject(strResult) as JObject;
+                    if (obj["result"].ToString().Equals("0"))   //没有结果或没有数据
+                    {
+                        intResult = 0;
+                    }
+                    else
+                    {
+                        intResult = int.Parse(obj["result"].ToString());
+                    }
+                    strResult = obj["data"].ToString();
+                }
+                catch (Exception exp)
+                {
+                    intResult = 0;
+                }
+            }
+            catch (Exception exp)
+            {
+                intResult = 0;
+            }
+            return intResult;
+        }
+
+
+        /// <summary>
+        /// 调用接口通用方法
+        /// </summary>
+        /// <param name="strUri">接口地址</param>
+        /// <param name="strPara">传入参数</param>
+        /// <param name="obj">返回反序列化的对象</param>
+        /// <returns>请求是否成功，1成功，0不成功</returns>
+        public static int RequestService<T>(string strUri, string strPara, ref T obj)
+        {
+            int intResult = 1;
+            string message = string.Empty;
+            try
+            {
+                HttpWebRequest myRequest = (HttpWebRequest)WebRequest.Create(strUri);
+
+                myRequest.Method = "POST";
+
+                myRequest.ContentType = "application/x-www-form-urlencoded";
+                if (strPara==null)
+                {
+                    //获得请求流
+                    Stream writer = myRequest.GetRequestStream();
+                    // 关闭请求流
+                    writer.Close();
+                }
+                else
+                {
+                    string paraUrlCoded = strPara;
+                    byte[] payload;
+                    //将URL编码后的字符串转化为字节
+                    payload = System.Text.Encoding.GetEncoding("utf-8").GetBytes(paraUrlCoded);
+                    //设置请求的 ContentLength 
+                    myRequest.ContentLength = payload.Length;
+                    //获得请求流
+                    Stream writer = myRequest.GetRequestStream();
+                    //将请求参数写入流
+                    writer.Write(payload, 0, payload.Length);
+                    // 关闭请求流
+                    writer.Close();
+                }
+
+                
+
+                try
+                {
+                    HttpWebResponse HttpWResp = (HttpWebResponse)myRequest.GetResponse();
+                    Stream myStream = HttpWResp.GetResponseStream();
+                    StreamReader sr = new StreamReader(myStream, Encoding.GetEncoding("utf-8"));
+                    string strResult = sr.ReadToEnd();
+
+                    //将json格式的字符串转换成实例
+                    JObject jobj = JsonConvert.DeserializeObject(strResult) as JObject;
+                    if (jobj["result"].ToString().Equals("0"))   //没有结果或没有数据
+                    {
+                        intResult = 0;
+                        obj = default(T);
+                    }
+                    else
+                    {
+                        intResult = int.Parse(jobj["result"].ToString());
+                        obj = JsonConvert.DeserializeObject<T>(jobj["data"].ToString());
+                    }
+                }
+                catch (Exception exp)
+                {
+                    intResult = 0;
+                }
+            }
+            catch (Exception exp)
+            {
+                intResult = 0;
+            }
+            return intResult;
+        }
+
+        /// <summary>
+        /// 反序列化对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public static T Deserialize<T>(string data)
+        {
+            return JsonConvert.DeserializeObject<T>(data);
+        }
+    }
+}
---
+++ 代扣定时服务/WindowsTimeService/Service1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using Tellyes.Common;
+using log4net;
+using System.Reflection;
+using System.Configuration;
+using System.IO;
+using System.Xml;
+using static System.Net.Mime.MediaTypeNames;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+namespace WindowsTimeService
+{
+   
+    public partial class Service1 : ServiceBase
+    {
+        ILog log = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+        System.Timers.Timer timer1;  //计时器
+        private Paramlist paramList=null;
+        public Service1()
+        {
+            
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            read();
+            timer1 = new System.Timers.Timer();
+            timer1.Interval = 1000;  //设置计时器事件间隔执行时间
+            timer1.Elapsed += new System.Timers.ElapsedEventHandler(timer1_Elapsed);
+            timer1.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            this.timer1.Enabled = false;
+        }
+        private void timer1_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            try
+            {
+                int intHour = e.SignalTime.Hour;
+                int intMinute = e.SignalTime.Minute;
+                int intSecond = e.SignalTime.Second;
+                string result = "";
+                if (paramList != null)
+                {
+                    foreach (var item in paramList.DataSource)
+                    {
+                        Param p = item as Param;
+                        if (intHour == int.Parse(p.Hour) && intMinute == int.Parse(p.Minute) && intSecond == 0)
+                        {
+                            InterfaceHelper.RequestService(p.Url, "", ref result);
+                            log.Info("调用: " + p.Url);
+                            log.Info("返回:" + result);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return;
+            }
+           
+
+        }
+        private void read()
+        {
+            try
+            {
+                string configPath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\timer.xml";
+                if (!File.Exists(configPath))
+                {
+                    log.Error("配置文件丢失，程序无法运行！");
+                    return;
+                }
+                FileStream fs = File.Open(configPath, FileMode.Open, FileAccess.Read);
+                StreamReader sr = new StreamReader(fs);
+                string text = sr.ReadToEnd();
+                var list = XmlSerializeHelper.DeSerialize<Paramlist>(text);
+                paramList = list as Paramlist;
+                sr.Close();
+                fs.Close();
+            }
+            catch (Exception ex)
+            {
+                log.Error(ex.Message);
+                return;
+            }
+           
+        }
+            
+    }
+}
